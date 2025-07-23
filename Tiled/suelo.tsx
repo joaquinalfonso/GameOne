d477cb34dd4573545d3dfea184d18d20fb102b9f
Joaquín Alfonso <joaquinalfonsogonzalez@gmail.com>
2025-07-23 21:10:03 +0200

    add missing files

---
+++ Tiled/suelo.tsx
@@ -0,0 +1,88 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="suelo" tilewidth="32" tileheight="32" spacing="1" margin="1" tilecount="16" columns="4">
+ <editorsettings>
+  <export target="../mapas/suelo..tsj definitivo.tsj" format="json"/>
+ </editorsettings>
+ <image source="tiled.png" width="150" height="150"/>
+ <tile id="0">
+  <objectgroup>
+   <object id="1" x="0" y="7" width="32" height="25"/>
+  </objectgroup>
+ </tile>
+ <tile id="1">
+  <objectgroup>
+   <object id="1" x="0" y="7" width="32" height="25"/>
+  </objectgroup>
+ </tile>
+ <tile id="2">
+  <objectgroup>
+   <object id="1" x="0" y="7" width="32" height="25"/>
+  </objectgroup>
+ </tile>
+ <tile id="3">
+  <objectgroup>
+   <object id="1" x="0" y="7" width="32" height="25"/>
+  </objectgroup>
+ </tile>
+ <tile id="4">
+  <objectgroup>
+   <object id="1" x="0" y="7" width="32" height="25"/>
+  </objectgroup>
+ </tile>
+ <tile id="5">
+  <objectgroup>
+   <object id="1" x="0" y="7" width="32" height="25"/>
+  </objectgroup>
+ </tile>
+ <tile id="6">
+  <objectgroup>
+   <object id="1" x="0" y="7" width="32" height="25"/>
+  </objectgroup>
+ </tile>
+ <tile id="7">
+  <objectgroup>
+   <object id="1" x="0" y="7" width="32" height="25"/>
+  </objectgroup>
+ </tile>
+ <tile id="8">
+  <objectgroup>
+   <object id="1" x="0" y="7" width="32" height="25"/>
+  </objectgroup>
+ </tile>
+ <tile id="9">
+  <objectgroup>
+   <object id="1" x="0" y="7" width="32" height="25"/>
+  </objectgroup>
+ </tile>
+ <tile id="10">
+  <objectgroup>
+   <object id="1" x="0" y="7" width="32" height="25"/>
+  </objectgroup>
+ </tile>
+ <tile id="11">
+  <objectgroup>
+   <object id="1" x="0" y="7" width="32" height="25"/>
+  </objectgroup>
+ </tile>
+ <tile id="12">
+  <objectgroup>
+   <object id="1" x="0" y="7" width="32" height="25"/>
+  </objectgroup>
+ </tile>
+ <tile id="13">
+  <objectgroup>
+   <object id="1" x="0" y="7" width="32" height="25"/>
+  </objectgroup>
+ </tile>
+ <tile id="14">
+  <objectgroup>
+   <object id="1" x="0" y="7" width="32" height="25"/>
+  </objectgroup>
+ </tile>
+ <tile id="15">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="7" width="32" height="25"/>
+   <object id="2" type="suelo" x="80" y="-52" height="53.3333"/>
+  </objectgroup>
+ </tile>
+</tileset>
